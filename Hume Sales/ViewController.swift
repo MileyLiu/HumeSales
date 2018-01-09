72012cce71827df34a7f60e6bd04c61e972f64ce
MileyLiu <mileyliu@sgr-lap-macjz.local>
2018-01-09 16:43:49 +1100

    Initial Commit

---
+++ Hume Sales/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hume Sales
+//
+//  Created by MileyLiu on 9/1/18.
+//  Copyright © 2018 MileyLiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
